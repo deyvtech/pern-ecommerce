58be371a5c6c35971e144f7919fe5e272148e89e
Deyb <92702144+deyvtech@users.noreply.github.com>
2026-03-18 23:20:10 +0800

    Fixed the email bug

--- be-app/src/schemas/user.schemas.ts
+++ be-app/src/schemas/user.schemas.ts
@@ -5,17 +5,17 @@ const registerSchema = z.object({
 		.string()
 		.min(3)
 		.max(100, "Username must be between 3 and 100 characters long"),
-	email: z.string().email("Invalid email address"),
+	email: z.string().email("Invalid email address").toLowerCase().trim(),
 	password: z.string().min(6, "Password must be at least 6 characters long"),
 });
 
 const loginSchema = z.object({
-	email: z.string().email("Invalid email address"),
+	email: z.string().email("Invalid email address").toLowerCase().trim(),
 	password: z.string().min(6, "Password must be at least 6 characters long"),
 });
 
 const forgotPasswordSchema = z.object({
-	email: z.string().email("Invalid email address"),
+	email: z.string().email("Invalid email address").toLowerCase().trim(),
 });
 const resetPasswordSchema = z.object({
 	password: z.string().min(6, "Password must be at least 6 characters long"),
